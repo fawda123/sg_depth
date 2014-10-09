6ca2a103e35c5243cc455f35655320b651d072ee
fawda123 <mbafs2012@gmail.com>
2014-10-09 18:19:14 -0500

    logistic growth model for TB, same as bigbend

--- extra.R
+++ extra.R
@@ -1,180 +1,122 @@
-######
-# old tampa bay test dataset
-
-######
-# load spatial data
-
-shps <- list.files('M:/GIS/seagrass', '^.*902.*shp$', full.names = T)
-
-# all secchi pts for old tampa bay
-secchi_shp <- readShapePoints(
-  shps[1],
-  proj4string = CRS("+proj=longlat +datum=WGS84")
-  )
+source('.Rprofile')
 
 # segment polygon for old tampa bay
-seg_shp <- readShapeSpatial(shps[2])
-
-# seagrass polygon coverage for old tampa bay
-sgpoly_shp <- readShapePoly(shps[3])
+seg_shp <- readShapeSpatial('seagrass_gis/seg_902.shp')
 
 # seagrass bathmetry intersected points for old tampa bay
-sgpts_shp <- readShapePoints(shps[4])
+sgpts_shp <- readShapeSpatial('seagrass_gis/sgpts_902_2010.shp')
 
-######
-# repeat depth of col analysis from Jim's paper
+# set ggplot theme
+theme_set(theme_bw())
 
-sg_prp <- sg_bins(sgpts_shp)
+# for debugging
+grid_spc <- 0.02
+grid_seed <- 1234
+test_pt <- 16
+radius <- 0.04
+thresh <- 0.1    
+show_all <- F
 
-# simple plot of proportion of 'occupied' points at depth bings
-to_plo <- sg_prp[40:54, ]
-to_plo <- melt(to_plo, id.var = 'bins')
-to_plo$growth_cat <- factor(to_plo$variable, levels = c('all', 'cont'), 
-  labels = c('All', 'Continous'))
-  
-all_mod <- max_est(sg_prp, 'all', mod = T)
-cont_mod <- max_est(sg_prp, 'cont', mod = T)
-
-all_est <- round(max_est(sg_prp, 'all'), 2)
-cont_est <- round(max_est(sg_prp, 'cont'), 2)
-
-cols  <- c('lightgreen', 'lightblue')
-linesz <- 1
-
-ggplot(to_plo, aes(x = bins, y = value, group = variable,
-  colour = growth_cat)) +
-  geom_line(size = linesz) +
-  geom_point(size = 4) +
-  geom_abline(
-    intercept = all_mod[1], 
-    slope = all_mod[2], 
-    linetype = 'dashed', 
-    colour = cols[1], 
-    size = linesz
-    ) +
-  geom_abline(
-    intercept = cont_mod[1], 
-    slope = cont_mod[2], 
-    linetype = 'dashed',
-    colour = cols[2],
-    size = linesz
-    ) +
-  ylab('Prop. of  points') +
-  ylab('Prop. of  points') +
-  xlab('Depth bin (m)') +
-  scale_colour_manual('Growth\ncategory', values = cols)
-
-# estimates for depth of col
-max_est(sg_prp, 'all')
-max_est(sg_prp, 'cont')
-
-##
-# repeat by estimating depth of col using points within a random location
-
-# random points
-pts <- grid_est(seg_shp, spacing = 0.02)
+# random points  
+set.seed(grid_seed)
+pts <- grid_est(seg_shp, spacing = grid_spc) 
 
 # point from random points for buffer
-test_pt <- pts[sample(1:length(pts), 1), ]
+test_pt <- pts[test_pt, ]
 
 # get bathym points around test_pt
-buff_pts <- buff_ext(sgpts_shp, test_pt)
-
-p1 <- ggplot(seg_shp, aes(long, lat)) + 
-  geom_polygon(fill = 'white') +
-  geom_path(color = 'black') +
-  theme_classic() +
-  coord_equal() +
-  geom_point(
-    data = data.frame(pts), 
-    aes(Var1, Var2), 
-    size = 3,
-    pch = 1
-    ) +
-  geom_point(
-    data = data.frame(buff_pts),
-    aes(lon, lat), 
-    colour = 'red', 
-    size = 1, 
-    alpha = 0.7
-    )
-
-# estimate depth of col for test_pts
-sg_prp <- sg_bins(buff_pts)
-
-# simple plot of proportion of 'occupied' points at depth bins
-to_plo <- sg_prp[(nrow(sg_prp) - 8):nrow(sg_prp), ]
-to_plo <- melt(to_plo, id.var = 'bins')
-to_plo$growth_cat <- factor(to_plo$variable, levels = c('all', 'cont'), 
-  labels = c('All', 'Continous'))
+buff_pts <- buff_ext(sgpts_shp, test_pt, buff = radius)
   
-all_mod <- max_est(sg_prp, 'all', mod = T)
-cont_mod <- max_est(sg_prp, 'cont', mod = T)
+##
+# example w/ actual data
 
-all_est <- round(max_est(sg_prp, 'all'), 2)
-cont_est <- round(max_est(sg_prp, 'cont'), 2)
+# get data used to estimate depth of col
+
+est_pts <- data.frame(buff_pts)
+est_pts$depth <- -1 * est_pts$depth
+est_pts$depth <- pmax(0, est_pts$depth)
+
+# data
+dat <- doc_est(est_pts, thresh = thresh, 
+	depth_var = 'depth', sg_var = 'Descript'
+	)
+
+# actual ests
+act_ests <- dat$ests
+
+# format estimate for plot title
+if(any(is.na(act_ests))){ act_ests <- 'Depth of col: Not estimable'
+} else { 
+	act_ests <- paste('Depth of col:', round(act_ests, 1), 'm')
+	}
 
 ##
-# plot pt prop ests
+# simple plot of points by depth, all pts and those with seagrass
+to_plo <- dat$data
+to_plo <- melt(to_plo, id.var = 'Depth', 
+	measure.var = c('dep_cum', 'sg_cum'))
+to_plo$variable <- factor(to_plo$variable, levels = c('dep_cum', 'sg_cum'), 
+                            labels = c('All', 'Seagrass'))
+
+to_plo2 <- dat$preds
+to_plo2 <- melt(to_plo2, id.var = 'Depth', 
+	measure.var = grep('dep_cum|sg_cum', names(to_plo2), value = T)
+	)
+to_plo2$variable <- factor(to_plo2$variable, 
+	levels = c('dep_cum', 'sg_cum'),
+	labels = c('All', 'Seagrass')
+)
+
 
 cols  <- c('lightgreen', 'lightblue')
 linesz <- 1
 
-p2 <- ggplot(to_plo, aes(x = bins, y = value, group = variable,
-  colour = growth_cat)) +
+p2 <- ggplot(to_plo2, aes(x = Depth, y = value, group = variable,
+                         colour = variable)) +
   geom_line(size = linesz) +
-  geom_point(size = 4) +
-  geom_abline(
-    intercept = all_mod[1], 
-    slope = all_mod[2], 
-    linetype = 'dashed', 
-    colour = cols[1], 
-    size = linesz
-    ) +
-  geom_abline(
-    intercept = cont_mod[1], 
-    slope = cont_mod[2], 
-    linetype = 'dashed',
-    colour = cols[2],
-    size = linesz
-    ) +
-  ylab('Prop. of  points') +
-  ylab('Prop. of  points') +
-  xlab('Depth bin (m)') +
-  scale_colour_manual('Growth\ncategory', values = cols) +
-  theme(legend.position = c(0.8, 0.8))
+	geom_point(data = to_plo, aes(x = Depth, y = value, group = variable, 
+		colour = variable)) +
+ 	ylab('Cumulative points') +
+  xlab('Depth (m)') +
+  scale_colour_manual('Point category', values = cols) +
+  theme(legend.position = c(0, 1), legend.justification = c(0,1))
 
-ests <- paste0('All: Zmax ', all_est[1], ' Z50 ', all_est[2], '\n', 
-  'Continuous: Zmax ', cont_est[1], ' Z50 ', cont_est[2])
+##
+# plot slope of cumulative point curves
 
-grid.arrange(p1, p2, 
-  ncol = 2, 
-  main = ests
-  ) 
+# treshold label for legend
+thresh_lab <- paste0(round(100 * thresh), '% of all')
 
-max_est(sg_bins(buff_pts), 'all')
+to_plo <- dat$preds
+to_plo <- melt(to_plo, id.var = 'Depth', 
+	measure.var = grep('dep_slo|sg_slo|Threshold', names(to_plo), value = T)
+	)
+to_plo$variable <- factor(to_plo$variable)
+to_plo$variable <- factor(to_plo$variable, 
+	levels = c('dep_slo', 'sg_slo', grep('Thresh', levels(to_plo$variable), value = T)),
+	labels = c('All', 'Seagrass', thresh_lab)
+)
 
-# get depth of col ests for each point
-maxd <- NULL
-for(i in 1:length(pts)){
-  
-  cat(i, '\t')
-  
-  eval_pt <- pts[i, ]
-  buff_pts <- buff_ext(sgpts_shp, eval_pt, buff = 0.05)
-  ests <- max_est(sg_bins(buff_pts), 'all')
-  
-  maxd <- rbind(maxd, ests)
-  
-  }
+col_pts <- rep(cols[1], nrow(to_plo))
+col_pts[to_plo$variable == 'All'] <- cols[2]
 
-sizes <- scales::rescale(maxd[, 1], c(0.5, 3))
-col.fun <- colorRampPalette(brewer.pal(9, 'RdBu'))
-col.pts <- col.fun(length(sizes))[rank(sizes)]
+p3 <- ggplot(to_plo, aes(x = Depth, y = value,
+		colour = variable, linetype = variable)) +
+	geom_line(size = linesz) +
+ 	ylab('CDF Slope') +
+  xlab('Depth (m)') +
+	scale_linetype_manual('Slope category', 
+		values = c('solid', 'solid', 'dashed')
+		) + 
+	scale_colour_manual('Slope category', 
+  	values = c(cols[2], cols[1], cols[2])
+  	) +
+  theme(legend.position = c(1, 1), legend.justification = c(1, 1))
 
-sp::plot(seg_shp)
-# points(pts, cex = sizes, col = col.pts, pch = 16)
-sizes <- scales::rescale(maxd[, 1], c(0.8, 2))
-text(data.frame(pts)[, 1], data.frame(pts)[,2],
-  labels = round(maxd[, 1], 2), cex = sizes, col = col.pts, pch = 16)
+##
+# combine all plots
 
+grid.arrange(p1,
+	arrangeGrob(p2, p3, ncol = 2), 
+	ncol = 1, heights = c(1.25, 1),
+	main = act_ests)
--- server.R
+++ server.R
@@ -10,7 +10,7 @@ library(rgeos)
 library(gridExtra)
 
 # functions to use
-source('funcs.r')
+source('funcs.R')
 
 # segment polygon for old tampa bay
 seg_shp <- readShapeSpatial('seagrass_gis/seg_902.shp')
@@ -48,8 +48,8 @@ shinyServer(function(input, output) {
 #     grid_spc <- 0.02
 #     grid_seed <- 12
 #     test_pt <- 1
-#     radius <- 0.05
-#     thresh  <- 0.1
+#     radius <- 0.02
+#  		thresh <- 0.1   	
 #     show_all <- F
     
     # input from ui
@@ -72,20 +72,20 @@ shinyServer(function(input, output) {
         
         eval_pt <- pts[i, ]
         buff_pts <- buff_ext(sgpts_shp, eval_pt, buff = radius)
-        ests <- max_est(data.frame(buff_pts), thresh = thresh, 
+      	est_pts <- data.frame(buff_pts)
+				est_pts$depth <- -1 * est_pts$depth
+        est_pts$depth <- pmax(0, est_pts$depth)
+        ests <- try(doc_est(est_pts, thresh = thresh, 
         								depth_var = 'depth', sg_var = 'Descript' 
-        								)
+        								)$ests)
+      	if('try-error' %in% class(ests)) ests <- NA
         maxd[[i]] <- ests
         
       }
       
-    	# combine in data frame for plotting
-			maxd <- data.frame(do.call('rbind', maxd))
-			maxd <- data.frame(
-				pts, 
-				-1 * maxd
-				)
-			
+			# combine in data frame for plotting
+			maxd <- data.frame(pts, zmax_all = do.call('c', maxd))
+    	
     	# get values for combined legend
 			rngs <- range(maxd$zmax_all, na.rm = T)
 			brks <- seq(rngs[1], rngs[2], length = 5)
@@ -161,68 +161,90 @@ shinyServer(function(input, output) {
     			pch = 1
     		)
       
-      # get data used to estimate depth of col
-			ests <- max_est(data.frame(buff_pts), thresh = thresh, 
-        								depth_var = 'depth', sg_var = 'Descript', 
-												dat_out = T)
-    	
     	##
-      # actual ests
-    	act_ests <- max_est(data.frame(buff_pts), thresh = thresh, 
-        								depth_var = 'depth', sg_var = 'Descript'
-        								)
-    	
-    	# format estimate for plot title
-			if(is.na(act_ests)){ act_ests <- 'Depth of col: Not estimable'
+     	# get data used to estimate depth of col
+			
+			est_pts <- data.frame(buff_pts)
+			est_pts$depth <- -1 * est_pts$depth
+      est_pts$depth <- pmax(0, est_pts$depth)
+			
+			# data
+			dat <- doc_est(est_pts, thresh = thresh, 
+				depth_var = 'depth', sg_var = 'Descript'
+				)
+			
+			# actual ests
+			act_ests <- dat$ests
+			
+			# format estimate for plot title
+			if(any(is.na(act_ests))){ act_ests <- 'Depth of col: Not estimable'
 			} else { 
-				act_ests <- paste('Depth of col:', -1 * round(act_ests, 1), 'm')
+				act_ests <- paste('Depth of col:', round(act_ests, 1), 'm')
 				}
-
-    	##
+			
+			##
 			# simple plot of points by depth, all pts and those with seagrass
-			to_plo <- ests
+			to_plo <- dat$data
 			to_plo <- melt(to_plo, id.var = 'Depth', 
 				measure.var = c('dep_cum', 'sg_cum'))
 			to_plo$variable <- factor(to_plo$variable, levels = c('dep_cum', 'sg_cum'), 
 			                            labels = c('All', 'Seagrass'))
-    	
+			
+			to_plo2 <- dat$preds
+			to_plo2 <- melt(to_plo2, id.var = 'Depth', 
+				measure.var = grep('dep_cum|sg_cum', names(to_plo2), value = T)
+				)
+			to_plo2$variable <- factor(to_plo2$variable, 
+				levels = c('dep_cum', 'sg_cum'),
+				labels = c('All', 'Seagrass')
+			)
+			
+			
 			cols  <- c('lightgreen', 'lightblue')
 			linesz <- 1
 			
-			p2 <- ggplot(to_plo, aes(x = -1 * Depth, y = value, group = variable,
+			p2 <- ggplot(to_plo2, aes(x = Depth, y = value, group = variable,
 			                         colour = variable)) +
 			  geom_line(size = linesz) +
+				geom_point(data = to_plo, aes(x = Depth, y = value, group = variable, 
+					colour = variable)) +
 			 	ylab('Cumulative points') +
 			  xlab('Depth (m)') +
-			  scale_colour_manual('Point category', values = cols) +
+			  scale_colour_manual('Point category', values = rev(cols)) +
 			  theme(legend.position = c(0, 1), legend.justification = c(0,1))
-
-    	##
-    	# plot slope of cumulative point curves
-    	
+			
+			##
+			# plot slope of cumulative point curves
+			
 			# treshold label for legend
-    	thresh_lab <- paste0(round(100 * thresh), '% of all')
-    	
-			to_plo <- ests
+			thresh_lab <- paste0(round(100 * thresh), '% of all')
+			
+			to_plo <- dat$preds
 			to_plo <- melt(to_plo, id.var = 'Depth', 
-				measure.var = c('dep_slo', 'sg_slo', 'slo_thr'))
+				measure.var = grep('dep_slo|sg_slo|Threshold', names(to_plo), value = T)
+				)
+			to_plo$variable <- factor(to_plo$variable)
 			to_plo$variable <- factor(to_plo$variable, 
-				levels = c('dep_slo', 'sg_slo', 'slo_thr'), 
-			  labels = c('All', 'Seagrass', thresh_lab))
-    	
-			p3 <- ggplot(to_plo, aes(x = -1 * Depth, y = value)) +
-			  geom_line(size = linesz, 
-			  	aes(group = variable, colour = variable, linetype = variable)
-			  	) +
+				levels = c('dep_slo', 'sg_slo', grep('Thresh', levels(to_plo$variable), value = T)),
+				labels = c('All', 'Seagrass', thresh_lab)
+			)
+			
+			p3 <- ggplot(to_plo, aes(x = Depth, y = value,
+					colour = variable, linetype = variable)) +
+				geom_line(size = linesz) +
 			 	ylab('CDF Slope') +
 			  xlab('Depth (m)') +
-			  scale_colour_manual('Slope category', values = c(cols, cols[1])) +
-				scale_linetype_manual('Slope category', values = c('solid', 'solid', 'dotted')) + 
+				scale_linetype_manual('Slope category', 
+					values = c('solid', 'solid', 'dashed')
+					) + 
+				scale_colour_manual('Slope category', 
+			  	values = c(cols[2], cols[1], cols[2])
+			  	) +
 			  theme(legend.position = c(1, 1), legend.justification = c(1, 1))
-
-    	##
+			
+			##
     	# combine all plots
-   
+
 			grid.arrange(p1,
 				arrangeGrob(p2, p3, ncol = 2), 
 				ncol = 1, heights = c(1.25, 1),
